df40bf6cf483178a699531bf45d4cd9a9a637c13
murari <tiwarithetiger11@gmail.com>
2018-02-09 10:34:16 -0500

    ic

--- microservices/ui/app/src/hasuraExamples/Auth.js
+++ microservices/ui/app/src/hasuraExamples/Auth.js
@@ -93,8 +93,8 @@ fetch(url, requestOptions)
 .then(function(result) {
 	console.log(result);
 	// To save the auth token received to offline storage
-	// var authToken = result.auth_token
-	// window.localStorage.setItem('HASURA_AUTH_TOKEN', authToken);
+	 var authToken = result.auth_token
+	window.localStorage.setItem('HASURA_AUTH_TOKEN', authToken);
 })
 .catch(function(error) {
 	console.log('Request Failed:' + error);
