c97ee61dcb2b5d93c4046b0cb6650454783c4a47
murari <tiwarithetiger11@gmail.com>
2018-02-10 10:28:13 -0500

    ic

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -19,6 +19,98 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', hasuraExamplesRouter);
 
+
+
+app.post("/pay",(req,res)=>{
+	
+	const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/success",
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Buy Books",
+                "sku": "001",
+                "price": "25.00",
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        },
+        "description": "This is the payment description."
+    }]
+};
+
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        console.log(payment);
+		
+		for(let i=0;i<payment.links.length;i++)
+		{
+			if(payment.links[i].rel=='approval_url')
+			{
+				res.redirect(payment.links[i].href);
+			}
+		}
+    }
+});
+
+
+
+
+})
+
+
+
+app.get('/success',(req,res)=>{
+	const payerId=req.query.PayerID;
+	const paymentId=req.query.paymentId;
+	
+	const execute_payment_json={
+		"payer_id":payerId,
+		"transactions":[{
+			"amount":{
+				"currency":"USD",
+				"total":"25.00"
+	               }
+		}]
+	};
+	
+	paypal.payment.execute(paymentId,execute_payment_json,function(error,payment){
+		if(error){
+			console.log(error.response);
+			throw error;
+		}else{
+			console.log(JSON.stringify(payment));
+			res.send('sucess');
+		}
+		
+	});
+})
+
+
+app.get('/cancel',(req,res)=>res.send('cancelled'));
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
