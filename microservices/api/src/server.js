2234c93c9c9f15d25e9e05838ea7456212d55430
murari <tiwarithetiger11@gmail.com>
2018-02-10 20:48:24 -0500

    ic

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -101,7 +101,7 @@ app.get('/success',(req,res)=>{
 			throw error;
 		}else{
 			console.log(JSON.stringify(payment));
-			res.send('<h1>Transaction Successful   </h1><a href="https://ui.aster61.hasura-app.io">Click here to continue</a>');
+			res.send('<h1>Transaction Successful </h1><a href="https://ui.aster61.hasura-app.io">Click here to continue</a>');
 		}
 		
 	});
